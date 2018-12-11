d3ae2507ff82cc5a7c8aa675291ecddc0477aefe
646547989 <646547989@qq.com>
2018-12-11 11:19:10 +0800

    完成用户认证

--- resources/views/index/layouts/app.blade.php
+++ resources/views/index/layouts/app.blade.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+    {{-- CSRF Token --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'BBS') - WeAPP</title>
 
-    <!-- Styles -->
+    {{-- Styles --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -29,7 +29,7 @@
     @include('index.layouts._footer')
 </div>
 
-<!-- Scripts -->
+{{-- Scripts --}}
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
